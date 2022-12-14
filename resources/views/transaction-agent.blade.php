88b00c79e63f0f06b31fb94e0a497f86bc66a80b
n0k1b <nokibevon7@gmail.com>
2022-12-14 18:13:07 +0600

    https remove

--- resources/views/transaction-agent.blade.php
+++ resources/views/transaction-agent.blade.php
@@ -81,7 +81,7 @@
         $('#transaction_record').hide()
         fetchAllTransaction()
     })
-    const base_url = 'https://slplay.xyz';
+    const base_url = 'http://slplay.xyz';
     //const base_url = 'http://bkash_copy.test';
     const token = "{{ Session::get('api_token') }}";
     const config = {
